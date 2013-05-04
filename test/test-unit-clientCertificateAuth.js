5cbd23e1562c5eb5501db7ee62d4cfe1854b687c
Tony Gies <tgies@tgies.net>
2013-05-03 21:34:17 -0500

    Enhanced/extra/corrected/more readable tests
    - Add a few tests for error conditions
    - Reword some existing tests
    - New mock requests to support new tests
    - More readable test code, hopefully

--- test/test-unit-clientCertificateAuth.js
+++ test/test-unit-clientCertificateAuth.js
@@ -9,14 +9,16 @@ var getMockPeerCertificate = function() {
       L: 'Waco',
       O: 'SHOE',
       OU: 'Upstairs',
-      CN: 'Proctor Davenport' },
+      CN: 'Proctor Davenport'
+    },
     issuer: {
       C: 'US',
       ST: 'Texas',
       L: 'Waco',
       O: 'SHOE',
       OU: 'Computers',
-      CN: 'SHOE Computers Dept. of Very Big Prime Numbers' },
+      CN: 'SHOE Computers Dept. of Very Big Prime Numbers' 
+    },
     valid_from: 'Jun 19 03:26:11 2004 GMT',
     valid_to:   'Jan 19 03:14:07 2038 GMT', // scheduled heat death of universe
     fingerprint: 'BA:DA:DD:EA:DB:EE:FC:CC:CC:CC:07:15:19:88:C0:FF:EE:00:12:00'
@@ -28,30 +30,62 @@ describe('clientCertificateAuth', function() {
     clientCertificateAuth.should.be.a('function').and.have.lengthOf(1);
   });
 
-  it('should return a function taking three arguments -- the middleware',
-      function() {
-          clientCertificateAuth(function() {}).should.be.a('function').and.have.lengthOf(3);
-      }
-    );
+  it('should return a function taking three arguments -- the middleware', function() {
+    clientCertificateAuth(function() {}).should.be.a('function').and.have.lengthOf(3);
+  });
 
   describe('(clientCertificateAuth(cb))(req, res, next)', function() {
-    var mockReq = { secure: true,
+    /*
+     * the objects mockGoodReq, mockUnsecureReq, and mockUnauthReq imitate the
+     * expected properties and behavior of the real req object of the type
+     * supported by this software, in the case of a secure and authorized
+     * request; an unsecure (cleartext) request; and a secure but unauthorized
+     * (untrusted or absent client certificate) request, respectively.
+     */
+    var mockGoodReq = {
+      secure: true,
       client: { authorized: true },
       connection: { getPeerCertificate: getMockPeerCertificate }
     };
+
+    var mockUnsecureReq = {
+      secure: false,
+      client: { authorized: false, },
+      connection: { getPeerCertificate: function() { return {}; } }
+    };
+
+    var mockUnauthReq = {
+      secure: true,
+      client: { authorized: false, },
+      connection: { getPeerCertificate: getMockPeerCertificate }
+    };
+
     var mockRes = {};
 
-    describe('when the request is secure and the client is authorized', function() {
-
+    describe('when the request is secure and the client certificate validates', function() {
       it('should call cb', function(done) {
-        (clientCertificateAuth(function() { done(); }))(mockReq, mockRes, function() {});
+        (clientCertificateAuth(function() { done(); }))(mockGoodReq, mockRes, function() {});
       });
 
-      it('should call next() with no arguments to pass control if cb(cert) returns true',
-        function(done) {
-          (clientCertificateAuth(function() { return true; }))(mockReq, mockRes, done);
-        }
-      );
+      it('should call next() to pass control successfully if cb(cert) returns true', function(done) {
+        (clientCertificateAuth(function() { return true; }))(mockGoodReq, mockRes, done);
+      });
+
+      it('should pass a 401 error to next() if cb(cert) returns false', function(done) {
+        (clientCertificateAuth(function() { return false; }))(mockGoodReq, mockRes, function(e) {
+          e.should.be.an.instanceOf(Error).and.have.property('status', 401);
+          done();
+        });
+      });
+    });
+
+    describe('when the request is secure but the client certificate does not validate', function() {
+      it('should pass a 401 error to next()', function(done) {
+        (clientCertificateAuth(function() { return true; }))(mockUnauthReq, mockRes, function(e) {
+          e.should.be.an.instanceOf(Error).and.have.property('status', 401);
+          done();
+        });
+      });
     });
   });
 });
