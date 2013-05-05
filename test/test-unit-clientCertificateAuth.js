2daf0d8be0f108485627eac3343e790a74a2b4a9
Tony Gies <tgies@tgies.net>
2013-05-04 20:42:25 -0500

    Add tests, fix empty cert bug, improve packaging
    This is version 0.2.1.
    
    - Add mostly complete unit testing using mock objects, and integrate
      testing with http://travis-ci.org.
    - Fix handling of an edge case where get PeerCertificate() returns an
      empty certificate. Close #1.
    - Improve package.json to be more complete and correct.
    
    Squashed commit of the following:
    
    commit 4139e2170c6d7d4ed6b8a847b9835c524a41febd
    Author: Tony Gies <tgies@tgies.net>
    Date:   Sat May 4 20:18:33 2013 -0500
    
        Handle the empty-cert-object case. Close #1.
    
        Fix the cert test to correctly handle the case that the object returned
        by req.getPeerCertificate() is an empty object. See
        https://github.com/tgies/client-cert-auth/issues/1 for more information.
    
    commit 1f59990fc5a06bf2e33cca76fe8a6ca862b1255f
    Author: Tony Gies <tgies@tgies.net>
    Date:   Fri May 3 23:43:18 2013 -0500
    
        Fix typos in test/test-unit-clientCertificateAuth.js
    
        - Copyedit comment.
        - Remove spurious comma in mockUn*Req hashes.
    
    commit 5cbd23e1562c5eb5501db7ee62d4cfe1854b687c
    Author: Tony Gies <tgies@tgies.net>
    Date:   Fri May 3 21:34:17 2013 -0500
    
        Enhanced/extra/corrected/more readable tests
    
        - Add a few tests for error conditions
        - Reword some existing tests
        - New mock requests to support new tests
        - More readable test code, hopefully
    
    commit 756b8cfd827f81783b7662f36f6375a5618b4676
    Author: Tony Gies <tgies@tgies.net>
    Date:   Fri May 3 15:46:56 2013 -0500
    
        Add http://travis-ci.org status image
    
    commit 5318934ebcacf25dcf174bb8245520cb818ae95a
    Merge: fe6bcf9 7186f17
    Author: Tony Gies <tgies@tgies.net>
    Date:   Fri May 3 15:21:45 2013 -0500
    
        Merge branch 'feature-travis' into develop
    
    commit 7186f1751ed0775629d8dd83adc4fbf8a140281a
    Author: Tony Gies <tgies@tgies.net>
    Date:   Fri May 3 15:20:55 2013 -0500
    
        Add .travis.yml for http://travis-ci.org
    
    commit 5e580108b54ae60840f0c6e02bc557cf7b94eb1f
    Author: Tony Gies <tgies@tgies.net>
    Date:   Fri May 3 14:17:24 2013 -0500
    
        More correct and complete package.json
    
        package.json now includes devDependencies, supports testing, and
        contains additional useful metadata.
    
    commit fe6bcf9cdd9c13bed6d8340642b20b933b410a0b
    Author: Tony Gies <tgies@tgies.net>
    Date:   Fri May 3 14:17:24 2013 -0500
    
        More correct and complete package.json
    
        package.json now includes devDependencies, supports testing, and
        contains additional useful metadata.
    
    commit 1c3127092a2de3c2238c24191245ec8bc9288cbb
    Author: Tony Gies <tgies@tgies.net>
    Date:   Fri May 3 00:58:31 2013 -0500
    
        i am not Fond of swapfiles.
    
    commit dc88decc5dec10602a9c991b982cb0f0b416519f
    Author: Tony Gies <tgies@tgies.net>
    Date:   Fri May 3 00:57:59 2013 -0500
    
        clarification
    
    commit e54155532f2d4119334f6e5fb5d7a7a9902753a7
    Author: Tony Gies <tgies@tgies.net>
    Date:   Fri May 3 00:37:01 2013 -0500
    
        very basic and incomplete unit testing

---
+++ test/test-unit-clientCertificateAuth.js
@@ -0,0 +1,91 @@
+var clientCertificateAuth = require('../lib/clientCertificateAuth.js');
+var should = require('should');
+
+var getMockPeerCertificate = function() {
+  return {
+    subject: {
+      C: 'US',
+      ST: 'Texas',
+      L: 'Waco',
+      O: 'SHOE',
+      OU: 'Upstairs',
+      CN: 'Proctor Davenport'
+    },
+    issuer: {
+      C: 'US',
+      ST: 'Texas',
+      L: 'Waco',
+      O: 'SHOE',
+      OU: 'Computers',
+      CN: 'SHOE Computers Dept. of Very Big Prime Numbers' 
+    },
+    valid_from: 'Jun 19 03:26:11 2004 GMT',
+    valid_to:   'Jan 19 03:14:07 2038 GMT', // scheduled heat death of universe
+    fingerprint: 'BA:DA:DD:EA:DB:EE:FC:CC:CC:CC:07:15:19:88:C0:FF:EE:00:12:00'
+  };
+};
+
+describe('clientCertificateAuth', function() {
+  it('should be a function taking one argument', function() {
+    clientCertificateAuth.should.be.a('function').and.have.lengthOf(1);
+  });
+
+  it('should return a function taking three arguments -- the middleware', function() {
+    clientCertificateAuth(function() {}).should.be.a('function').and.have.lengthOf(3);
+  });
+
+  describe('(clientCertificateAuth(cb))(req, res, next)', function() {
+    /*
+     * The objects mockGoodReq, mockUnsecureReq, and mockUnauthReq imitate the
+     * expected properties and behavior of the real request object of the type
+     * supported by this software, in the case of a secure and authorized
+     * request; an unsecure (cleartext) request; and a secure but unauthorized
+     * (untrusted or absent client certificate) request, respectively.
+     */
+    var mockGoodReq = {
+      secure: true,
+      client: { authorized: true },
+      connection: { getPeerCertificate: getMockPeerCertificate }
+    };
+
+    var mockUnsecureReq = {
+      secure: false,
+      client: { authorized: false },
+      connection: { getPeerCertificate: function() { return {}; } }
+    };
+
+    var mockUnauthReq = {
+      secure: true,
+      client: { authorized: false },
+      connection: { getPeerCertificate: getMockPeerCertificate }
+    };
+
+    var mockRes = {};
+
+    describe('when the request is secure and the client certificate validates', function() {
+      it('should call cb', function(done) {
+        (clientCertificateAuth(function() { done(); }))(mockGoodReq, mockRes, function() {});
+      });
+
+      it('should call next() to pass control successfully if cb(cert) returns true', function(done) {
+        (clientCertificateAuth(function() { return true; }))(mockGoodReq, mockRes, done);
+      });
+
+      it('should pass a 401 error to next() if cb(cert) returns false', function(done) {
+        (clientCertificateAuth(function() { return false; }))(mockGoodReq, mockRes, function(e) {
+          e.should.be.an.instanceOf(Error).and.have.property('status', 401);
+          done();
+        });
+      });
+    });
+
+    describe('when the request is secure but the client certificate does not validate', function() {
+      it('should pass a 401 error to next()', function(done) {
+        (clientCertificateAuth(function() { return true; }))(mockUnauthReq, mockRes, function(e) {
+          e.should.be.an.instanceOf(Error).and.have.property('status', 401);
+          done();
+        });
+      });
+    });
+  });
+});
